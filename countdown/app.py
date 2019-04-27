60b43f1814509143d9eeb96c458ebabeb341eef9
Unknown <jon.howell314@gmail.com>
2019-04-27 14:34:26 +0800

    take in n-large number

--- countdown/app.py
+++ countdown/app.py
@@ -1,22 +1,19 @@
 from countdown.solver import Solver, Expression
 from countdown.generator import get_numbers
+import sys
 
 
-def main():
-    n_large = 3
-
+def run(n_large):
     selection, target = get_numbers(n_large)
     print('Numbers are: {} -> {}'.format(selection, target))
 
     solver = Solver()
-
     best_answer = solver.answers(selection, target)
-
-    print('Best answer found: {}'.format(best_answer.value))
 
     input('Hit enter to get solution')
 
     print('--- Solution --- ')
+    print('Best Answer: {}'.format(best_answer.value))
     print('Numbers Used: {}'.format(best_answer.numbers()))
     for step in best_answer.ordered_steps():
         print(step)
@@ -24,6 +21,12 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    args = sys.argv
+
+    if len(args) < 2:
+        print('I need a number')
+        exit(-1)
+
+    run(int(args[1]))
 
 
